9e96c13594f5d9d76c2f7527aab5c9cf038196ea
Huseyin Tekin <huseyintekin@garantibarter.com.tr>
2022-05-21 02:40:47 +0300

    northwind

---
+++ src/main/java/projects/northwind/business/abstracts/ProductService.java
@@ -0,0 +1,33 @@
+package projects.northwind.business.abstracts;
+
+import java.util.List;
+
+import projects.northwind.core.utilities.results.DataResult;
+import projects.northwind.core.utilities.results.Result;
+import projects.northwind.entities.concretes.Product;
+
+public interface ProductService {
+	
+	DataResult<List<Product>> getAll();
+	
+	DataResult<List<Product>> getAll(int pageNo, int pageSize);
+	
+	DataResult<List<Product>> getAllSorted();
+	
+	Result add(Product product);
+	
+	DataResult<Product> getByProductName(String productName);
+	
+	DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId);
+	
+	DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId);
+	
+	DataResult<List<Product>> getByCategoryIn(List<Integer> categories);
+	
+	DataResult<List<Product>> getByProductNameContains(String productName);
+	
+	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+	
+	DataResult<List<Product>> getByNameAndCategoryId(String productName, int categoryId);
+
+}
---
+++ src/main/java/projects/northwind/entities/concretes/Category.java
@@ -0,0 +1,81 @@
+package projects.northwind.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="categories")
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","products"})
+public class Category {
+	@Id
+	@Column(name="category_id")
+	private int categoryId;
+	
+	@Column(name="category_name")
+	private String categoryName;
+	
+	@Column(name="description")
+	private String description;
+	
+	@OneToMany(mappedBy = "category")
+	private List<Product> products;
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public Category() {
+		super();
+	}
+
+	public Category(int categoryId, String categoryName, String description, List<Product> products) {
+		super();
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+		this.description = description;
+		this.products = products;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
